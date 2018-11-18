735703928be8e15bb93ac8d88eaeb1a8e7482f43
fiveseven5577 <38633786+fiveseven5577@users.noreply.github.com>
2018-11-18 13:39:38 +0800

    Create onedeck.R

---
+++ onedeck.R
@@ -0,0 +1,17 @@
+deck<-data.frame(
+  face=c("king","queen","jack","ten","nine","eight","seven","six","five","four","three","two","ace",
+         "king","queen","jack","ten","nine","eight","seven","six","five","four","three","two","ace",
+         "king","queen","jack","ten","nine","eight","seven","six","five","four","three","two","ace",
+         "king","queen","jack","ten","nine","eight","seven","six","five","four","three","two","ace"),
+  
+  suit=c("spades","spades","spades","spades","spades","spades","spades","spades","spades","spades","spades","spades","spades",
+         "clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs","clubs",
+         "diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds","diamonds",
+         "hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts","hearts"),
+  
+  value=c(13,12,11,10,9,8,7,6,5,4,3,2,1,
+          13,12,11,10,9,8,7,6,5,4,3,2,1,
+          13,12,11,10,9,8,7,6,5,4,3,2,1,
+          13,12,11,10,9,8,7,6,5,4,3,2,1)
+  
+)
